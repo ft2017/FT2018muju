04c28b5e6035bc928b7f88485702f108f02c7c7d
zhangtao <zhangtao@localhost.localdomain>
2018-01-13 15:04:47 +0800

    addxx

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -30,7 +30,7 @@ class Muju(models.Model):
    # muju_plan_date = models.CharField('模具预计完成日',max_length=100, default='.')
     muju_plan_date = models.DateField('模具预计完成日',null=True, blank=True)
     muju_seq = models.CharField('项次',max_length=10, default='.')
-    muju_Reason = models.DateField('报修原因',null=True, blank=True)
+    muju_Reason = models.DateField('x报修原因',null=True, blank=True)
     muju_date1 = models.DateField('拆模计划日期',null=True, blank=True)
     muju_date2 = models.DateField('拆模实际日期',null=True, blank=True)
     muju_date3 = models.DateField('易损与材料制作计划',null=True, blank=True)
