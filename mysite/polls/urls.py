7cf5c2337b7481c713bd619fc32d671491e7617f
zhangtao <zhangtao@localhost.localdomain>
2018-01-16 15:53:16 +0800

    url get set

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,11 +10,14 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     path('dev', views.indexDev, name='indexDev'),
-    path('<int:muju_id>/', views.index_old, name='index_old'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('index_old', views.index_old, name='index_old'),
+
+     #ex: /polls/5/
+    #path('<int:question_id>/', views.detail, name='detail'),
+
+    path('<int:muju_wo>/', views.index_old, name='index_old'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+   # path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+   # path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
